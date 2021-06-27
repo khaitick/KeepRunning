de8ecde90c9c2b57d0b8c6e51f507fe0828e738e
caden hl <62642278+khaitick@users.noreply.github.com>
2021-06-27 23:11:12 +0800

    Complete lesson 3.4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //public float speed = 10f;
     public float jumpForce;
     public float gravityModifier;
     public bool gameOver = false;
+    public ParticleSystem collisionFX;
+    public ParticleSystem runningFX;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
+    private float speed;
     private bool isOnGround;
     private Rigidbody playerRB;
+    private Animator anim;
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        speed = FindObjectOfType<MoveLeft>().speed;
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            anim.SetTrigger("Jump_trig");
+            runningFX.Stop();
+            playerAudio.PlayOneShot(jumpSound);
         }
         //transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
@@ -35,12 +47,20 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            anim.SetFloat("Speed_f", speed);
+            anim.SetBool("Static_b", true);
+            runningFX.Play();
             print("Ground");
         }
         else if (collision.gameObject.CompareTag("Obstacles"))
         {
             gameOver = true;
             print("Game over");
+            anim.SetBool("Death_b", true);
+            anim.SetInteger("DeathType_int", 1);
+            runningFX.Stop();
+            collisionFX.Play();
+            playerAudio.PlayOneShot(crashSound);
         }
     }
 }
